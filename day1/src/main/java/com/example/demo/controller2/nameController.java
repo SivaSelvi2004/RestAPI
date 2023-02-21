15e13ce8dc8119e4a42cb22635adcb92f9bb30bb
SivaSelvi2004 <ssivaselvis1132004@gmail.com>
2023-02-21 14:15:26 +0530

    RestAPI
    day3
    
    RestAPI
    
    RestAPI

---
+++ day1/src/main/java/com/example/demo/controller2/nameController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class nameController {
+	String studentName="IamNeo!";
+	@GetMapping("/neo")
+	public String getName() {
+		return "Welcome "+studentName;
+	}
+}
